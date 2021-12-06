8af8a0f2e0b4066e0635cd1443eaba43f45e0590
Jakob Murko <sraka1@users.noreply.github.com>
2021-12-06 23:58:41 +0100

    Fixes

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (errorClass: any, handler: (error: any, ctx: any) => void) => any;
-export default _default;
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+declare type HandlerFunction = (error: any, ctx: any) => void;
+declare const _default: (errorClass: any, handler: HandlerFunction) => any;
+export default _default;
